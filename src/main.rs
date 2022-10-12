f259898a01a0848c6a920373cc5c8893bacde1d1
Morten Bergseng <bergseng.m@gmail.com>
2022-10-12 15:38:26 +0200

    rendering chart

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use std::{
+    ops::{Add, Sub},
+    rc::Rc,
+};
+
+use chrono::{Duration, Utc};
 use reqwasm::http::Request;
 use yew::prelude::*;
+use yew_chart::{
+    axis::{Axis, Orientation, Scale},
+    linear_axis_scale::LinearScale,
+    series::{self, Labeller, Series, Tooltipper, Type},
+    time_axis_scale::TimeScale,
+};
 
 mod common;
+
+const WIDTH: f32 = 533.0;
+const HEIGHT: f32 = 300.0;
+const MARGIN: f32 = 50.0;
+const TICK_LENGTH: f32 = 10.0;
 
 #[function_component(CurrentComponent)]
 fn current_component(props: &common::CurrentComponentProps) -> Html {
@@ -9,13 +26,82 @@ fn current_component(props: &common::CurrentComponentProps) -> Html {
     html! {
         <div class="current">
             <div class="pointbench">{"points on bench: \t"} {current.points_on_bench}</div>
-            <div class="bank">{"money in bank: \t"}{current.bank}</div>
+            <div class="bank">{"money in bank: \t"}{current.bank as f64 / 10.0}</div>
             <div class="teamvalue">{"team value: \t"}{current.value as f64 / 10.0}</div>
             <div class="transfersandcost">{"transfers: "}{current.event_transfers}{"    transfer cost: "}{current.event_transfers_cost}</div>
             <div class="rank">{"rank: \t"}{current.overall_rank}</div>
             <div class="points">{"points: \t"}{current.points}</div>
             <div class="totpoints">{"total points: \t"}{current.total_points}</div>
         </div>
+    }
+}
+
+#[function_component(ChartComponent)]
+fn chart_component() -> Html {
+    let end_date = Utc::now();
+    let start_date = end_date.sub(Duration::days(4));
+    let timespan = start_date..end_date;
+
+    let circle_text_labeller = Rc::from(series::circle_text_label("Label")) as Rc<dyn Labeller>;
+
+    let data_set = Rc::new(vec![
+        (start_date.timestamp_millis(), 1.0, None),
+        (
+            start_date.add(Duration::days(1)).timestamp_millis(),
+            4.0,
+            None,
+        ),
+        (
+            start_date.add(Duration::days(2)).timestamp_millis(),
+            3.0,
+            None,
+        ),
+        (
+            start_date.add(Duration::days(3)).timestamp_millis(),
+            2.0,
+            None,
+        ),
+        (
+            start_date.add(Duration::days(4)).timestamp_millis(),
+            5.0,
+            Some(circle_text_labeller),
+        ),
+    ]);
+
+    let h_scale = Rc::new(TimeScale::new(timespan, Duration::days(1))) as Rc<dyn Scale<Scalar = _>>;
+    let v_scale = Rc::new(LinearScale::new(0.0..5.0, 1.0)) as Rc<dyn Scale<Scalar = _>>;
+
+    let tooltip = Rc::from(series::y_tooltip()) as Rc<dyn Tooltipper<_, _>>;
+
+    html! {
+            <svg class="chart" viewBox={format!("0 0 {} {}", WIDTH, HEIGHT)} preserveAspectRatio="none">
+                <Series<i64, f32>
+                    series_type={Type::Line}
+                    name="some-series"
+                    data={data_set}
+                    horizontal_scale={Rc::clone(&h_scale)}
+                    horizontal_scale_step={Duration::days(2).num_milliseconds()}
+                    tooltipper={Rc::clone(&tooltip)}
+                    vertical_scale={Rc::clone(&v_scale)}
+                    x={MARGIN} y={MARGIN} width={WIDTH - (MARGIN * 2.0)} height={HEIGHT - (MARGIN * 2.0)} />
+
+                <Axis<f32>
+                    name="some-y-axis"
+                    orientation={Orientation::Left}
+                    scale={Rc::clone(&v_scale)}
+                    x1={MARGIN} y1={MARGIN} xy2={HEIGHT - MARGIN}
+                    tick_len={TICK_LENGTH}
+                    title={"Some Y thing".to_string()} />
+
+                <Axis<i64>
+                    name="some-x-axis"
+                    orientation={Orientation::Bottom}
+                    scale={Rc::clone(&h_scale)}
+                    x1={MARGIN} y1={HEIGHT - MARGIN} xy2={WIDTH - MARGIN}
+                    tick_len={TICK_LENGTH}
+                    title={"Some X thing".to_string()} />
+
+            </svg>
     }
 }
 
@@ -55,14 +141,24 @@ fn app_component() -> Html {
                 .collect(),
             None => html! {
                 <>
-                    <div> {String::from("error")}</div>
+                <div> {String::from("error")}</div>
                 </>
             },
         }
     })
 }
 
+#[function_component(Render)]
+pub fn render() -> Html {
+    html! {
+        <>
+        <App />
+        <ChartComponent />
+        </>
+    }
+}
+
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
-    yew::start_app::<App>();
+    yew::start_app::<Render>();
 }
